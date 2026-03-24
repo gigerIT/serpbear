ae19978add7b2db69e024821e5ce653c23e0e811
gigerit <info@gigerit.ch>
2026-03-24 13:34:41 +0100

    fix: check releases from fork repo

--- components/settings/Changelog.tsx
+++ components/settings/Changelog.tsx
@@ -1,80 +1,110 @@
-import React, { useLayoutEffect, useMemo } from 'react';
-import TimeAgo from 'react-timeago';
-import dayjs from 'dayjs';
-import SidePanel from '../common/SidePanel';
-import { useFetchChangelog } from '../../services/misc';
-import Icon from '../common/Icon';
+import React, { useLayoutEffect, useMemo } from "react";
+import TimeAgo from "react-timeago";
+import dayjs from "dayjs";
+import SidePanel from "../common/SidePanel";
+import { useFetchChangelog } from "../../services/misc";
+import Icon from "../common/Icon";
 
-const Markdown = React.lazy(() => import('react-markdown'));
+const Markdown = React.lazy(() => import("react-markdown"));
+const GITHUB_REPO_URL = "https://github.com/gigerIT/serpbear";
 
 type ChangeLogProps = {
-   closeChangeLog: Function,
-}
+  closeChangeLog: Function;
+};
 
 const ChangeLogloader = () => {
-   return (
-      <div className='w-full h-full absolute flex justify-center items-center'>
-         <Icon type="loading" size={36} color='#999' />
-      </div>
-   );
+  return (
+    <div className="w-full h-full absolute flex justify-center items-center">
+      <Icon type="loading" size={36} color="#999" />
+    </div>
+  );
 };
 
 const ChangeLog = ({ closeChangeLog }: ChangeLogProps) => {
-   const { data: changeLogData, isLoading } = useFetchChangelog();
+  const { data: changeLogData, isLoading } = useFetchChangelog();
 
-   useLayoutEffect(() => {
-      document.body.style.overflow = 'hidden';
-      // eslint-disable-next-line no-unused-expressions
-      () => {
-         console.log('run CleanUp !');
-         document.body.style.overflow = 'auto';
-       };
-   }, []);
+  useLayoutEffect(() => {
+    document.body.style.overflow = "hidden";
+    // eslint-disable-next-line no-unused-expressions
+    () => {
+      console.log("run CleanUp !");
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
-   const onClose = () => {
-      document.body.style.overflow = 'auto';
-      closeChangeLog();
-   };
+  const onClose = () => {
+    document.body.style.overflow = "auto";
+    closeChangeLog();
+  };
 
-   const changeLogs = useMemo(() => {
-      if (changeLogData && Array.isArray(changeLogData)) {
-         return changeLogData.map(({ name = '', body, published_at }:{name: string, body: string, published_at: string}) => ({
-            version: name,
-            major: !!(name.match(/v\d+\.0+\.0/)),
-            date: published_at,
-            content: body.replaceAll(/^(##|###) \[([^\]]+)\]\(([^)]+)\) \(([^)]+)\)/g, '')
-            .replaceAll(/\(\[(.*?)\]\((https:\/\/github\.com\/towfiqi\/serpbear\/commit\/([a-f0-9]{40}))\)\)/g, ''),
-         }));
-      }
-      return [];
-   }, [changeLogData]);
+  const changeLogs = useMemo(() => {
+    if (changeLogData && Array.isArray(changeLogData)) {
+      return changeLogData.map(
+        ({
+          name = "",
+          body,
+          published_at,
+        }: {
+          name: string;
+          body: string;
+          published_at: string;
+        }) => ({
+          version: name,
+          major: !!name.match(/v\d+\.0+\.0/),
+          date: published_at,
+          content: body
+            .replaceAll(/^(##|###) \[([^\]]+)\]\(([^)]+)\) \(([^)]+)\)/g, "")
+            .replaceAll(
+              /\(\[(.*?)\]\((https:\/\/github\.com\/(towfiqi|gigerIT)\/serpbear\/commit\/([a-f0-9]{40}))\)\)/g,
+              ""
+            ),
+        })
+      );
+    }
+    return [];
+  }, [changeLogData]);
 
-   return <SidePanel title='SerpBear Changelog' closePanel={onClose}>
-            <React.Suspense fallback={<ChangeLogloader />}>
-               {!isLoading && changeLogs.length > 0 && (
-                  <div className='changelog-body bg-[#f8f9ff] px-6 pt-4 pb-10 overflow-y-auto styled-scrollbar'>
-                     {changeLogs.map(({ version, content, date, major }) => {
-                        return (
-                           <div
-                           key={version}
-                           className={`domKeywords bg-white rounded mb-6 border ${major ? ' border-indigo-400' : 'border-transparent'}`}>
-                              <h4 className=' px-5 py-3 border-b border-b-gray-100 flex justify-between text-indigo-700 font-semibold'>
-                                 <a href={`https://github.com/towfiqi/serpbear/releases/tag/${version}`}>
-                                    {version} {major && <span className=' text-xs bg-amber-100 text-amber-700 px-2 py-1 rounded ml-2'>Major</span>}
-                                 </a>
-                                 <span className=' text-sm text-gray-500'>
-                                    <TimeAgo title={dayjs(date).format('DD-MMM-YYYY, hh:mm:ss A')} date={date} />
-                                 </span>
-                              </h4>
-                              <div className='changelog-content px-5 py-3 text-sm text-left'><Markdown>{content}</Markdown></div>
-                           </div>
-                        );
-                     })}
+  return (
+    <SidePanel title="SerpBear Changelog" closePanel={onClose}>
+      <React.Suspense fallback={<ChangeLogloader />}>
+        {!isLoading && changeLogs.length > 0 && (
+          <div className="changelog-body bg-[#f8f9ff] px-6 pt-4 pb-10 overflow-y-auto styled-scrollbar">
+            {changeLogs.map(({ version, content, date, major }) => {
+              return (
+                <div
+                  key={version}
+                  className={`domKeywords bg-white rounded mb-6 border ${
+                    major ? " border-indigo-400" : "border-transparent"
+                  }`}
+                >
+                  <h4 className=" px-5 py-3 border-b border-b-gray-100 flex justify-between text-indigo-700 font-semibold">
+                    <a href={`${GITHUB_REPO_URL}/releases/tag/${version}`}>
+                      {version}{" "}
+                      {major && (
+                        <span className=" text-xs bg-amber-100 text-amber-700 px-2 py-1 rounded ml-2">
+                          Major
+                        </span>
+                      )}
+                    </a>
+                    <span className=" text-sm text-gray-500">
+                      <TimeAgo
+                        title={dayjs(date).format("DD-MMM-YYYY, hh:mm:ss A")}
+                        date={date}
+                      />
+                    </span>
+                  </h4>
+                  <div className="changelog-content px-5 py-3 text-sm text-left">
+                    <Markdown>{content}</Markdown>
                   </div>
-               )}
-               {isLoading && <ChangeLogloader />}
-            </React.Suspense>
-         </SidePanel>;
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {isLoading && <ChangeLogloader />}
+      </React.Suspense>
+    </SidePanel>
+  );
 };
 
 export default ChangeLog;
--- services/misc.tsx
+++ services/misc.tsx
@@ -1,10 +1,17 @@
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
+
+const GITHUB_REPO = "gigerIT/serpbear";
 
 export async function fetchChangelog() {
-   const res = await fetch('https://api.github.com/repos/towfiqi/serpbear/releases', { method: 'GET' });
-   return res.json();
+  const res = await fetch(
+    `https://api.github.com/repos/${GITHUB_REPO}/releases`,
+    { method: "GET" }
+  );
+  return res.json();
 }
 
 export function useFetchChangelog() {
-   return useQuery('changelog', () => fetchChangelog(), { cacheTime: 60 * 60 * 1000 });
+  return useQuery("changelog", () => fetchChangelog(), {
+    cacheTime: 60 * 60 * 1000,
+  });
 }
