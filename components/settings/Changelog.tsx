5b9a76e92ccb8bff7a080f640a7d278c251c9749
gigerit <info@gigerit.ch>
2026-03-24 13:47:45 +0100

    fix: restore changelog cleanup and lint compliance

--- components/settings/Changelog.tsx
+++ components/settings/Changelog.tsx
@@ -1,12 +1,12 @@
-import React, { useLayoutEffect, useMemo } from "react";
-import TimeAgo from "react-timeago";
-import dayjs from "dayjs";
-import SidePanel from "../common/SidePanel";
-import { useFetchChangelog } from "../../services/misc";
-import Icon from "../common/Icon";
+import React, { useLayoutEffect, useMemo } from 'react';
+import TimeAgo from 'react-timeago';
+import dayjs from 'dayjs';
+import SidePanel from '../common/SidePanel';
+import { useFetchChangelog } from '../../services/misc';
+import Icon from '../common/Icon';
 
-const Markdown = React.lazy(() => import("react-markdown"));
-const GITHUB_REPO_URL = "https://github.com/gigerIT/serpbear";
+const Markdown = React.lazy(() => import('react-markdown'));
+const GITHUB_REPO_URL = 'https://github.com/gigerIT/serpbear';
 
 type ChangeLogProps = {
   closeChangeLog: Function;
@@ -14,8 +14,8 @@ type ChangeLogProps = {
 
 const ChangeLogloader = () => {
   return (
-    <div className="w-full h-full absolute flex justify-center items-center">
-      <Icon type="loading" size={36} color="#999" />
+    <div className='w-full h-full absolute flex justify-center items-center'>
+      <Icon type='loading' size={36} color='#999' />
     </div>
   );
 };
@@ -24,16 +24,15 @@ const ChangeLog = ({ closeChangeLog }: ChangeLogProps) => {
   const { data: changeLogData, isLoading } = useFetchChangelog();
 
   useLayoutEffect(() => {
-    document.body.style.overflow = "hidden";
-    // eslint-disable-next-line no-unused-expressions
-    () => {
-      console.log("run CleanUp !");
-      document.body.style.overflow = "auto";
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'auto';
     };
   }, []);
 
   const onClose = () => {
-    document.body.style.overflow = "auto";
+    document.body.style.overflow = 'auto';
     closeChangeLog();
   };
 
@@ -41,7 +40,7 @@ const ChangeLog = ({ closeChangeLog }: ChangeLogProps) => {
     if (changeLogData && Array.isArray(changeLogData)) {
       return changeLogData.map(
         ({
-          name = "",
+          name = '',
           body,
           published_at,
         }: {
@@ -53,47 +52,47 @@ const ChangeLog = ({ closeChangeLog }: ChangeLogProps) => {
           major: !!name.match(/v\d+\.0+\.0/),
           date: published_at,
           content: body
-            .replaceAll(/^(##|###) \[([^\]]+)\]\(([^)]+)\) \(([^)]+)\)/g, "")
+            .replaceAll(/^(##|###) \[([^\]]+)\]\(([^)]+)\) \(([^)]+)\)/g, '')
             .replaceAll(
               /\(\[(.*?)\]\((https:\/\/github\.com\/(towfiqi|gigerIT)\/serpbear\/commit\/([a-f0-9]{40}))\)\)/g,
-              ""
+              '',
             ),
-        })
+        }),
       );
     }
     return [];
   }, [changeLogData]);
 
   return (
-    <SidePanel title="SerpBear Changelog" closePanel={onClose}>
+    <SidePanel title='SerpBear Changelog' closePanel={onClose}>
       <React.Suspense fallback={<ChangeLogloader />}>
         {!isLoading && changeLogs.length > 0 && (
-          <div className="changelog-body bg-[#f8f9ff] px-6 pt-4 pb-10 overflow-y-auto styled-scrollbar">
+          <div className='changelog-body bg-[#f8f9ff] px-6 pt-4 pb-10 overflow-y-auto styled-scrollbar'>
             {changeLogs.map(({ version, content, date, major }) => {
               return (
                 <div
                   key={version}
                   className={`domKeywords bg-white rounded mb-6 border ${
-                    major ? " border-indigo-400" : "border-transparent"
+                    major ? ' border-indigo-400' : 'border-transparent'
                   }`}
                 >
-                  <h4 className=" px-5 py-3 border-b border-b-gray-100 flex justify-between text-indigo-700 font-semibold">
+                  <h4 className=' px-5 py-3 border-b border-b-gray-100 flex justify-between text-indigo-700 font-semibold'>
                     <a href={`${GITHUB_REPO_URL}/releases/tag/${version}`}>
-                      {version}{" "}
+                      {version}{' '}
                       {major && (
-                        <span className=" text-xs bg-amber-100 text-amber-700 px-2 py-1 rounded ml-2">
+                        <span className=' text-xs bg-amber-100 text-amber-700 px-2 py-1 rounded ml-2'>
                           Major
                         </span>
                       )}
                     </a>
-                    <span className=" text-sm text-gray-500">
+                    <span className=' text-sm text-gray-500'>
                       <TimeAgo
-                        title={dayjs(date).format("DD-MMM-YYYY, hh:mm:ss A")}
+                        title={dayjs(date).format('DD-MMM-YYYY, hh:mm:ss A')}
                         date={date}
                       />
                     </span>
                   </h4>
-                  <div className="changelog-content px-5 py-3 text-sm text-left">
+                  <div className='changelog-content px-5 py-3 text-sm text-left'>
                     <Markdown>{content}</Markdown>
                   </div>
                 </div>
